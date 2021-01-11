7c39abc0ca90df17c72516cfa3257a4f21008da0
YanchaoMa <mayc01@jsfund.cn>
2021-01-11 10:28:16 +0800

    使用Nginx-Http发送CYPHER请求测试

--- src/test/java/data/lab/ongdb/etl/compose/NeoComposerTest_4.java
+++ src/test/java/data/lab/ongdb/etl/compose/NeoComposerTest_4.java
@@ -7,6 +7,9 @@ package data.lab.ongdb.etl.compose;
 
 import com.alibaba.fastjson.JSONObject;
 import data.lab.ongdb.etl.common.CRUD;
+import data.lab.ongdb.http.HttpRequestProxy;
+import data.lab.ongdb.http.extra.HttpProxyRequest;
+import data.lab.ongdb.http.extra.HttpRequest;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
@@ -66,6 +69,30 @@ public class NeoComposerTest_4 {
             String cypher = "MATCH (n) RETURN n LIMIT 10";
             JSONObject result = this.neoComposer.execute(cypher, CRUD.RETRIEVE);
             System.out.println(result);
+        }
+    }
+
+    @Test
+    public void http_01() {
+        HttpProxyRequest httpRequest = new HttpProxyRequest("default", "ongdb", "datalab%pro");
+        for (; ; ) {
+            /**
+             * 使用Nginx地址
+             * **/
+            String url = "http://10.20.13.200/db/data/transaction/commit";
+            String cypher = "{\n" +
+                    "    \"statements\": [\n" +
+                    "        {\n" +
+                    "            \"statement\": \"MATCH p=(n)--() RETURN p LIMIT 25\",\n" +
+                    "            \"resultDataContents\": [\n" +
+                    "                \"row\",\n" +
+                    "                \"graph\"\n" +
+                    "            ]\n" +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}";
+            String result = httpRequest.httpPost(url, cypher);
+            System.out.println(JSONObject.parseObject(result));
         }
     }
 }
